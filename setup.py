bd1f96005335460903e9e27102102914836effa5
Emmanuel Peralta <eperalta@webgeoservices.com>
2016-01-11 16:10:15 +0100

    Adds requirements.txt and setup.py.

---
+++ setup.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+requirements = parse_requirements('requirements.txt')
+
+setup(
+    name="jsdocgen",
+    version="0.0.1",
+    license="Private",
+    url="https://github.com/WebGeoServices/jsdocgen",
+    packages=find_packages('.'),
+    include_package_data=True,
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2.7"
+    ],
+    scripts=[
+        'js-doc-gen'
+    ],
+    install_requires=[str(requirement.req) for requirement in requirements]
+)
