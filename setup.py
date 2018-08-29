f6d6d818c15c63d5a56f6447e99cde7e134e9140
Emmanuel Peralta <manz@users.noreply.github.com>
2018-08-29 17:52:29 +0200

    Replace parse_requirements with an ad hoc implementation.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 # coding=utf-8
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
-requirements = parse_requirements('requirements.txt', session=False)
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+requirements = parse_requirements('requirements.txt')
 
 setup(
     name="jsdocgen",
